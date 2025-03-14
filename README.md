d66ab985a822111f02a54e8cda55430a0cbf81ee
Luis Eduardo <pplpierre20@gmail.com>
2025-03-14 20:18:43 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# collegeapi2025" 
---
+++ src/test/java/br/ueg/college/collegeAPI/CollegeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ueg.college.collegeAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
